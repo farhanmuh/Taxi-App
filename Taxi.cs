6ae1282fd475ec986af74eff61a398c24c22c377
farhanmuh <97308802+farhanmuh@users.noreply.github.com>
2022-03-23 21:21:52 +0700

    Tugas Praktikum 3
    class taxi

---
+++ Taxi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApplication
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassengers { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0} ", DriverName);
+            Console.WriteLine("On Duty : {0} ", OnDuty);
+            Console.WriteLine("Number of Passengers : {0}", NumPassengers);
+        }
+
+        public void PickUpPassengers()
+        {
+            Console.WriteLine(DriverName+" sedang menjemput penumpang");
+        }
+        public void DropOffPassengers()
+        {
+            Console.WriteLine(DriverName+" selesai mengantar penumpang");
+        }
+    } 
+}
