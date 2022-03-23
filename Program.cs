c5586da06b79b2cfe8a64ed4cc0f887bb159b0da
farhanmuh <97308802+farhanmuh@users.noreply.github.com>
2022-03-23 21:23:20 +0700

    Tugas Praktirkum 3
    coding program taxi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassengers = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassengers();
+            taxi.DropOffPassengers();
+
+            Console.ReadKey();
+        }
+    }
+}
